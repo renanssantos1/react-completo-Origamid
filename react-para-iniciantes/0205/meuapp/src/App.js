b0a0423a6b558ff504f4d24d1fd26b4bc5506b5f
Renan da silva santos <renan.serverti@gmail.com>
2020-07-26 12:18:45 -0300

    aula 0208-2 deixando o app limpo

--- react-para-iniciantes/0205/meuapp/src/App.js
+++ react-para-iniciantes/0205/meuapp/src/App.js
@@ -1,11 +1,6 @@
 import React from "react";
-import Aula02082 from "./components/0208_2";
 
 const App = () => {
-  return (
-    <>
-      <Aula02082 />
-    </>
-  );
+  return <></>;
 };
 export default App;
