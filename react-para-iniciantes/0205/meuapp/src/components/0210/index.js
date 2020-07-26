b93b29c94a7a3b579fe6bc20f7739d8b01034272
Renan da silva santos <renan.serverti@gmail.com>
2020-07-26 13:08:25 -0300

    aula 0210 aula sobre componentes

---
+++ react-para-iniciantes/0205/meuapp/src/components/0210/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+import Header from "./Header";
+import Footer from "./Footer";
+import Form from "./Form/Form";
+
+function Aula0210() {
+  return (
+    <>
+      <Header />
+      <Form />
+      <Footer />
+    </>
+  );
+}
+
+export default Aula0210;
