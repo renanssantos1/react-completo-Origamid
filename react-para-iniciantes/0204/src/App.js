23910b5ac23a5b95baede9a57e6a3d3baba8c6db
Renan da silva santos <renan.serverti@gmail.com>
2020-07-26 09:23:30 -0300

    final da aula de introducao de configuracao

--- react-para-iniciantes/0204/src/App.js
+++ react-para-iniciantes/0204/src/App.js
@@ -2,7 +2,11 @@ import React from "react";
 import "./App.css";
 
 const App = () => {
-  return <a href="https://www.origamid.com">Origamid</a>;
+  return (
+    <div>
+      <a href="https://www.origamid.com">Origamid</a>;
+    </div>
+  );
 };
 
 export default App;
