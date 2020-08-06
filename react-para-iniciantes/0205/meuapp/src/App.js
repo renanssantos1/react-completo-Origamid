b3b5b142b3f4305a6293563c0808bf473d738c3e
Renan da silva santos <renan.serverti@gmail.com>
2020-08-06 19:41:38 -0300

    deixado o padrao limpo de novo

--- react-para-iniciantes/0205/meuapp/src/App.js
+++ react-para-iniciantes/0205/meuapp/src/App.js
@@ -1,12 +1,6 @@
 import React from "react";
-import UseContext02 from "./components/0305/UseContext02";
-import { ProdutoProvider } from "./ProdutoContext";
 
 const App = () => {
-  return (
-    <ProdutoProvider>
-      <UseContext02 />
-    </ProdutoProvider>
-  );
+  return <div></div>;
 };
 export default App;
