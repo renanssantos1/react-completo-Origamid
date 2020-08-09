2bd5cf7f0a5b97f831deccd2259dcdb988d98086
Renan da silva santos <renan.serverti@gmail.com>
2020-08-09 19:27:01 -0300

    usefetch adicionado no app

--- react-para-iniciantes/0205/meuapp/src/App.js
+++ react-para-iniciantes/0205/meuapp/src/App.js
@@ -1,6 +1,11 @@
 import React from "react";
+import CustomHooks02 from "./components/0306/CustomHooks02";
 
 const App = () => {
-  return <div></div>;
+  return (
+    <div>
+      <CustomHooks02 />
+    </div>
+  );
 };
 export default App;
