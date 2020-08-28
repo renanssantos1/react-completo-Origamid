a81e7ce783839314a75b6206fac4ed1c0aeca445
Renan da silva santos <renan.serverti@gmail.com>
2020-08-28 18:12:37 -0300

    criando um hook personalizado para helmet

--- react-para-iniciantes/0205/meuapp/src/pages/0601/Home.js
+++ react-para-iniciantes/0205/meuapp/src/pages/0601/Home.js
@@ -1,9 +1,11 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Head from "./Head";
 
 function Home() {
   return (
     <div>
+      <Head title="Origamid | Home" description="Curso de Oridamid" />
       <h1>Sobre</h1>
       <p>Essa e a home</p>
       <li>
